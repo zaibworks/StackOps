fae96b84b783de3deddd3911a3d2795ecbddbed7
m0hammadzaib <zaibfr4@gmail.com>
2026-04-30 19:23:37 +0530

    Today's Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-    <div className="bg-black h-screen w-screen">
+    <div className="bg-black h-screen w-screen p-10">
       <div className="text-white">Hey Zaib</div>
     </div>
     </>
