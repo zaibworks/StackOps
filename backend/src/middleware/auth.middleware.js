f19cf989aadfd28fae96bc2a9ff7752e890538d8
m0hammadzaib <zaibfr4@gmail.com>
2026-05-04 08:12:49 +0530

    Created authMiddleware

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+
+const authMiddleware=(req,res,next)=>{
+try{
+    const authHeader = req.headers.authorization
+
+    if(!authHeader){
+        res.status(401).json({message:'No token provided'})
+    }
+    const token = authHeader.split('')[1]
+    const decoded = jwt.verify(token,process.env.JWT_SECRET)
+}
+}
