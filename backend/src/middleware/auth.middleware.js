061727da291b0482156ccaf077189bfa206a24fd
m0hammadzaib <zaibfr4@gmail.com>
2026-05-04 08:16:32 +0530

    authMiddleware done

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -9,5 +9,12 @@ try{
     }
     const token = authHeader.split('')[1]
     const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+    req.user = decoded
+    next()
+}catch(error){
+   res.status(401).json({message:"Invalid token"})
 }
 }
+
+export default authMiddleware
