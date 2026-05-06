e0cbfd168572374ca64901e83dac6e45f9d66370
m0hammadzaib <zaibfr4@gmail.com>
2026-05-06 20:01:38 +0530

    Task module completed

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import authRoutes from '../modules/auth/auth.routes.js'
 import userRoutes from '../modules/user/user.routes.js'
+import taskRoutes from '../modules/task/task.routes.js'
 
 const app = express()
 const PORT = 3000;
@@ -9,6 +10,7 @@ app.use(express.json())
 
 app.use('/auth', authRoutes)
 app.use('user',userRoutes)
+app.use('/task',taskRoutes)
 
 app.get('/',(req,res)=>{
   res.send('Hey bro app is running')
