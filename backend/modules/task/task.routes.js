d5016b9fb52ea3748ed022b7a6812641de7ca2a4
m0hammadzaib <zaibfr4@gmail.com>
2026-05-14 18:56:27 +0530

    getTask routes

--- backend/modules/task/task.routes.js
+++ backend/modules/task/task.routes.js
@@ -2,14 +2,14 @@ import express from 'express'
 const router = express.Router()
 import authMiddleware from '../middleware/auth.middleware.js'
 import { createTaskController,
-    getTaskController,
+    getTasksController,
     updateTaskController,
     deleteTaskController 
 } from './task.controller.js'
 
 import {workspaceMemberMiddlware} from '../middleware/workspace.middleware.js'
 
-router.get('./',authMiddleware,getTaskController)
+router.get('/:workspaceId',authMiddleware,workspaceMemberMiddlware,getTasksController)
 router.post('/:workspaceId',authMiddleware,workspaceMemberMiddlware,createTaskController)
 router.put('/:id',authMiddleware,updateTaskController)
 router.delete('/:id',authMiddleware,deleteTaskController)
