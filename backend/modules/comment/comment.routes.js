249bc77ea6720612fdc2893555c928f3cfff79a5
m0hammadzaib <zaibfr4@gmail.com>
2026-05-15 16:06:51 +0530

    comment.routes.js completed

--- backend/modules/comment/comment.routes.js
+++ backend/modules/comment/comment.routes.js
@@ -1,1 +1,9 @@
- 
+import express from "express";
+const router = express.Router()
+import { addCommentController,getCommentsController,deleteCommentController } from "./comment.controller.js";
+import { workspaceMemberMiddlware } from "../middleware/workspace.middleware.js";
+import authMiddleware from "../middleware/auth.middleware.js";
+
+router.post('/:taskId',authMiddleware,workspaceMemberMiddlware,addCommentController)
+router.get('/:taskId',authMiddleware,workspaceMemberMiddlware,getCommentsController)
+router.delete('/:workspaceId/:commentId',authMiddleware,workspaceMemberMiddlware,deleteCommentController)
