07cfb6ee12a2c7b38dcc3ae2bfd835e1dc3f3778
m0hammadzaib <zaibfr4@gmail.com>
2026-05-16 11:25:08 +0530

    Completed validation bugs and extra codes

--- backend/modules/middleware/validation.middleware.js
+++ backend/modules/middleware/validation.middleware.js
@@ -1,32 +1,15 @@
 import {z} from 'zod'
 
-export const validate =(schema)=>{
+export const validate =(schema,errorMessage = "Validation failed")=>{
     return (req,res,next)=>{
         const result = schema.safeParse(req.body)
 
         if(!result.success){
             return res.status(400).json({
-                message:"Validation failed",
+                message:errorMessage ,
                 error: result.error.issues.map(issue=> ({
                     field: issue.path[0],
-                    message: issues.message
-                }))
-            })
-        }
-        req.body = result.data
-        next()
-    }
-}
-export const naming = (schema)=>{
-    return (req,res,next)=>{
-        const result = schema.safeParse(req.body)
-
-        if(!result.success){
-            return res.status(400).json({
-                message:"Workspace Creation failed",
-                error: result.error.issues.map(issue=>({
-                    field: issue.path[0],
-                    message:issue.message
+                    message: issue.message
                 }))
             })
         }
@@ -35,21 +18,4 @@ export const naming = (schema)=>{
     }
 }
 
-export const taskCheck = (schema)=>{
-    return (req,res,next)=>{
-    const result = schema.safeParse(req.body)
-
-     if(!result.success){
-            return res.status(400).json({
-                message:"Task Creation Failed",
-                error: result.error.issues.map(issue=>({
-                    field: issue.path[0],
-                    message:issue.message
-                }))
-            })
-        }
-        req.body = result.data
-        next()
-    }
-}
 
--- backend/modules/workspace/workspace.routes.js
+++ backend/modules/workspace/workspace.routes.js
@@ -9,17 +9,17 @@ import { createWorkspaceController,
 } from './workspace.controller.js'
 
 import { workspaceMemberMiddlware } from '../middleware/workspace.middleware.js'
-import { naming } from '../middleware/validation.middleware.js'
+import { validate} from '../middleware/validation.middleware.js'
 import { createWorkspaceSchema, updateWorkspaceSchema } from './workspace.schema.js'
 
 const router = express.Router()
 
-router.post('/',authMiddlware,naming(createWorkspaceSchema),createWorkspaceController)
-router.get('/',authMiddlware,naming(updateWorkspaceSchema) ,getMyWorkspaceController)
+router.post('/',authMiddlware,validate(createWorkspaceSchema,"Workspace creation failed"),createWorkspaceController)
+router.get('/',authMiddlware,getMyWorkspaceController)
 router.post('/:workspaceId/invite',authMiddlware,inviteMemberController)
 router.get('/:workspaceId/members',authMiddlware,workspaceMemberMiddlware,getWorkspaceMembersController)
 router.delete('/:workspaceId/members/:memberId',authMiddlware,workspaceMemberMiddlware,removeMemberController)
-router.put('/:workspaceId', authMiddlware, workspaceMemberMiddlware, updateWorkspaceController)
+router.put('/:workspaceId', authMiddlware,validate(updateWorkspaceSchema,"Workspace updation failed") ,workspaceMemberMiddlware, updateWorkspaceController)
 
 
 export default router
