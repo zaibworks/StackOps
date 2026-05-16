df746b7e4a4a369012a187b56f7cee14e09d4d0e
m0hammadzaib <zaibfr4@gmail.com>
2026-05-16 10:32:48 +0530

    Workspace naming completed with zod

--- backend/modules/middleware/validation.middleware.js
+++ backend/modules/middleware/validation.middleware.js
@@ -17,3 +17,39 @@ export const validate =(schema)=>{
         next()
     }
 }
+export const naming = (schema)=>{
+    return (req,res,next)=>{
+        const result = schema.safeParse(req.body)
+
+        if(!result.success){
+            return res.status(400).json({
+                message:"Workspace Creation failed",
+                error: result.error.issues.map(issue=>({
+                    field: issue.path[0],
+                    message:issue.message
+                }))
+            })
+        }
+        req.body = result.data
+        next()
+    }
+}
+
+export const taskCheck = (schema)=>{
+    return (req,res,next)=>{
+    const result = schema.safeParse(req.body)
+
+     if(!result.success){
+            return res.status(400).json({
+                message:"Task Creation Failed",
+                error: result.error.issues.map(issue=>({
+                    field: issue.path[0],
+                    message:issue.message
+                }))
+            })
+        }
+        req.body = result.body
+        next()
+    }
+}
+
--- backend/modules/workspace/workspace.routes.js
+++ backend/modules/workspace/workspace.routes.js
@@ -9,11 +9,13 @@ import { createWorkspaceController,
 } from './workspace.controller.js'
 
 import { workspaceMemberMiddlware } from '../middleware/workspace.middleware.js'
+import { naming } from '../middleware/validation.middleware.js'
+import { createWorkspaceSchema, updateWorkspaceSchema } from './workspace.schema.js'
 
 const router = express.Router()
 
-router.post('/',authMiddlware,createWorkspaceController)
-router.get('/',authMiddlware,getMyWorkspaceController)
+router.post('/',authMiddlware,naming(createWorkspaceSchema),createWorkspaceController)
+router.get('/',authMiddlware,naming(updateWorkspaceSchema) ,getMyWorkspaceController)
 router.post('/:workspaceId/invite',authMiddlware,inviteMemberController)
 router.get('/:workspaceId/members',authMiddlware,workspaceMemberMiddlware,getWorkspaceMembersController)
 router.delete('/:workspaceId/members/:memberId',authMiddlware,workspaceMemberMiddlware,removeMemberController)
