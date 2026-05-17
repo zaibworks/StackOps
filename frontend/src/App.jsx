bf3c6b72aed142c8d212d220ad900df1f70220ac
m0hammadzaib <zaibfr4@gmail.com>
2026-05-17 23:05:00 +0530

    Created pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+
+
 
 function App() {
   return (
-    <>
-    <div className="bg-black h-screen w-screen p-10">
-      <div className="text-white">Hey Zaib Brudd</div>
-    </div>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -14,3 +14,4 @@ api.interceptors.request.use((config)=>{
     return config
 })
 
+export default api
