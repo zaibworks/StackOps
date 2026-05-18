48bc147242617227cb355c2e9d3d53ebabd14448
m0hammadzaib <zaibfr4@gmail.com>
2026-05-18 14:30:45 +0530

    Writing all needed states

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+import api from '../api/axios.js'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState('')
+  const [error, setError] = useState('')
+
+  const navigate = useNavigate()
   return (
     <div>
-      Login
+
     </div>
   )
 }
