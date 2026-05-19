38d1e8b2e8102ffddcb685e3f4c8b55f79839908
m0hammadzaib <zaibfr4@gmail.com>
2026-05-19 15:57:15 +0530

    handleSubmit functoin started

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,10 +5,19 @@ import api from '../api/axios.js'
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState('')
+  const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
 
   const navigate = useNavigate()
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    setLoading(true)
+    setError('')
+    try{
+      const
+    }
+  }
   return (
     <div>
 
