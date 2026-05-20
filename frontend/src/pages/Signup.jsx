d0e209cb2e238b07f2bf3391e2aa5bb3bdf0d45d
m0hammadzaib <zaibfr4@gmail.com>
2026-05-20 11:35:38 +0530

    Signup Logic completed

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import api from '../api/axios.js'
 
 const Signup = () => {
+   const [email, setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [name, setName] = useState('')
+  const [error,setError]=useState('')
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const handleSignup = async (e)=>{
+       e.preventDefault()
+       setLoading(true)
+       setError('')
+       try{
+        const response = await api.post('/auth/signup',{
+          name,
+          email,
+          password
+        })
+          const token = response.data.token
+          localStorage.setItem('token',token)
+          navigate('/login')
+       }catch(e){
+       setError(e.response?.message ||'Signup failed')
+       }finally{
+        setLoading(false)
+       }
+  }
   return (
     <div>
-      Signup
+      <h1>Signup in StackOps</h1>
+      {error&& <p>{error}</p> }
+      <form onSubmit={handleSignup}>
+        <input type="text" placeholder='Name' value={name} onChange={(e)=>setName(e.target.value)} />
+        <input type="email" placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)} />
+        <input type="password" placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)} />
+        <button type='submit' disabled={loading}>{loading?'Signing In...':'Signup'}</button>
+      </form>
     </div>
   )
 }
