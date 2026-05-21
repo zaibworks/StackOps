27863fcdbf4c8d2b57cfd47bb1685ec71b319707
m0hammadzaib <zaibfr4@gmail.com>
2026-05-21 21:14:53 +0530

    Bug fixed and logout option added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useState,useEffect } from "react"
+import api from '../api/axios.js'
+import { useNavigate } from "react-router-dom"
+
 
 const Dashboard = () => {
+  const [workspaces, setWorkspaces] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchWorkspaces = async()=>{
+       const response = await api.get('/workspace')
+       setWorkspaces(response.data)
+       setLoading(false)
+    }
+    fetchWorkspaces()
+  }, [])
+
+  const handleLogout = ()=>{
+    localStorage.removeItem('token')
+    navigate('/login')
+  }
+  
   return (
     <div>
-      Dashboard
+      <h1>DashBoard</h1>
+     {workspaces.map((w)=>(
+      <div key={w.id}>
+        <p>{w.name}</p>
+      </div>
+     ))}
+
+     <button onClick={handleLogout} className="bg-red-600 rounded-2xl p-2 font-bold">Logout</button>
     </div>
   )
 }
