9b48246625740d5c8c2fb5c1bce119ac0dd432ff
m0hammadzaib <zaibfr4@gmail.com>
2026-05-24 23:53:34 +0530

    Task Creation in Workspace

--- frontend/src/pages/Workspace.jsx
+++ frontend/src/pages/Workspace.jsx
@@ -9,6 +9,12 @@ const Workspace = () => {
   const [loading, setLoading] = useState(true)
   const [task, setTask] = useState([])
   const [error, setError] = useState(false)
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [priority, setPriority] = useState('medium')
+  const [status, setStatus] = useState('todo')
+  const [dueDate, setdueDate] = useState('')
+  const [assignedToId, setAssignedToId] = useState('')
   
   const {workspaceId} = useParams()
 
@@ -30,7 +36,14 @@ const addTask = async()=>{
   e.preventDefault()
   setError('')
   try {
-     const response = await api.post(`/task/${workspaceId}`)
+     const response = await api.post(`/task/${workspaceId}`,{
+        title,
+        content,
+        priority,
+        satus,
+        dueDate:dueDate ? new Date(dueDate).toISOString() : undefined,
+        assignedToId:assignedToId ? parseInt(assignedToId) : undefined,
+     })
   setTask(response.data.data)
   } catch (err) {
      if(error){
@@ -60,6 +73,32 @@ const addTask = async()=>{
         <p>{t.status}</p>
       </div>
     ))}
+
+    <div>
+      <form onSubmit={addTask}>
+         <input type="text" placeholder='Task title' value={title} onChange={(e)=>setTitle(e.target.value)} />
+         <input type="text" placeholder='Task content' value={content} onChange={(e)=>setContent(e.target.value)} />
+         <select value={priority} onChange={(e)=>setPriority(e.target.value)}>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+         </select>
+         <select value={status} onChange={(e)=>e.target.value}>
+          <option value="todo"></option>
+          <option value="inprogress"></option>
+          <option value="completed"></option>
+
+          <input type="date" value={dueDate} onChange={(e) => setdueDate(e.target.value)}/>
+
+          <select value={assignedToId} onChange={(e)=>setAssignedToId(e.target.value)}></select>
+           <option value="">Unassigned</option>
+           {workspace?.members?.map((m)=>(
+            <option key={m.user.id} value={m.user.id}>{m.user.name}</option>
+           ))}
+         </select>
+          <button type='submit' className='bg-red-700'>Add Task</button>
+      </form>
+    </div>
     </div>
   )
 }
