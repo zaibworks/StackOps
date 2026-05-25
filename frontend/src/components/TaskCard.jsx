d710fe6e9d605ca9528185f71c5f6dadb343f274
m0hammadzaib <zaibfr4@gmail.com>
2026-05-25 10:45:09 +0530

    Update task function completed

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,17 +1,61 @@
 import React from 'react'
 import api from '../api/axios.js'
+import { useState } from 'react'
 
 const TaskCard = ({task,workspaceId,onTaskUpdate}) => {
+    const [isEditing, setisEditing] = useState(false)
+     const [editTitle, setEditTitle] = useState(task.title)
+    const [editStatus, setEditStatus] = useState(task.status)
+     const [editPriority, setEditPriority] = useState(task.priority)
 
     const deleteTask = async()=>{
       await api.delete(`/task/${workspaceId}/${task.id}`)
        await onTaskUpdate()
     }
+
+    const updateTask = async()=>{
+        await api.put(`/task/${workspaceId}/${task.id}`,{
+             title: editTitle,
+             priority: editPriority,
+           status: editStatus
+        })
+        await onTaskUpdate()
+        setisEditing(false)
+    }
+
   return (
     <div>
-      <p>{task.title}</p>
-      <p>{task.status}</p>
-     <button onClick={deleteTask}>Delete</button>
+        {isEditing?(
+            <div>
+                <form onSubmit={updateTask}>
+                         <input type="text" placeholder='Task title' value={editTitle} onChange={(e)=>setEditTitle(e.target.value)} />
+
+      <select value={editPriority} onChange={(e)=>setEditPriority(e.target.value)}>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+         </select>
+
+      <select value={editStatus} onChange={(e)=>setEditStatus(e.target.value)}>
+          <option value="todo">Todo</option>
+          <option value="inprogress">In-Progress</option>
+          <option value="done">Done</option>
+         </select>
+
+         <button type='Submit'>Done</button>
+                </form>
+
+<button onClick= {()=>setisEditing(false)}>Cancel</button>
+            </div>
+        ):(
+            <div>
+         <p>{task.title}</p>
+         <p>{task.status}</p>
+        <button onClick={deleteTask}>Delete</button>
+        <button onClick={()=>setisEditing(true)}>Edit</button>
+            </div>
+        )
+    }
     </div>
   )
 }
