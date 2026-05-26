2ac59b3b47212cd6d38cbbaad29bab34cecbd75e
m0hammadzaib <zaibfr4@gmail.com>
2026-05-27 01:27:00 +0530

    Card headers

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -40,7 +40,11 @@ const Signup = () => {
            <div className='absolute bottom-[-10rem] right-[-10rem] h-[25rem] w-[25rem] rounded-full bg-cyan-500/20 blue-[100px]'/>
       </div>
       <div className='z-10 w-full max-w-md rounded-3xl border border-zinc-800 border-zinc-900/70 px-8 py-10 backdrop-blur-xl shadow-[0_0_50px_rgba(0,0,0,0.4) transition-all duration-300 hover:border-zinc-700'>
+      <div className='mb-8 text-center'>
       <h1>Signup in StackOps</h1>
+      <h2></h2>
+      <p></p>
+      </div>
       {error&& <p>{error}</p> }
       <form onSubmit={handleSignup}>
         <input type="text" placeholder='Name' value={name} onChange={(e)=>setName(e.target.value)} />
