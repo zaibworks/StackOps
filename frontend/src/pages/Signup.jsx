fa33610fca10bca444caa1fc39560307e1598151
m0hammadzaib <zaibfr4@gmail.com>
2026-05-27 01:14:48 +0530

    Signup page background

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -32,6 +32,13 @@ const Signup = () => {
   }
   return (
     <div className='relative min-h-screen flex justify-center items-center bg-zinc-950 overflow-hidden px-4'>
+      <div className='absolute inset-0'>
+           <div className='absolute inset-0 opacity-[0.03]'>
+      <div className='h-full w-full bg-[linear-gradient(to_right,#ffffff_1px,transparent_1px),linear-gradient(to_bottom,#ffffff_1px,transparent_1px)] bg-[size:40px 40px]'/>
+           </div>
+           <div className='absolute top-[-10rem] left-[-10rem] h-[30rem] w-[30rem] rounded-full bg-cyan-400/20 blur-[100px]'/>
+           <div className='absolute bottom-[-10rem] right-[-10rem] h-[25rem] w-[25rem] rounded-full bg-cyan-500/20 blue-[100px]'/>
+      </div>
       <h1>Signup in StackOps</h1>
       {error&& <p>{error}</p> }
       <form onSubmit={handleSignup}>
