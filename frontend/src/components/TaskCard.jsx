fb4df580af037afa041cad3ed0a5df9088fb1d2e
m0hammadzaib <zaibfr4@gmail.com>
2026-06-11 14:19:32 +0530

    designing edit meta data section

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,62 +1,64 @@
-import React from 'react'
-import api from '../api/axios.js'
-import { useState } from 'react'
+import React from "react";
+import api from "../api/axios.js";
+import { useState } from "react";
 
-import { X, Calendar, User, Flag, PencilIcon } from "lucide-react"
+import { X, Calendar, User, Flag, PencilIcon } from "lucide-react";
 
-const TaskCard = ({task,workspace,onClose,onTaskUpdate}) => {
-    const [isEditing, setisEditing] = useState(false)
+const TaskCard = ({ task, workspace, onClose, onTaskUpdate }) => {
+  const [isEditing, setisEditing] = useState(false);
 
-     const [editTitle, setEditTitle] = useState(task.title)
-     const [editContent, setEditContent] = useState(task.content)
+  const [editTitle, setEditTitle] = useState(task.title);
+  const [editContent, setEditContent] = useState(task.content);
 
-     const [editDueDate, setEditDueDate] = useState(task.dueDate?new Date(task.dueDate).toISOString().split("T")[0]:"")
-   const [editAssignedToId, setEditAssignedToId] = useState(task.assignedTo?.id || "")
-     const [editPriority, setEditPriority] = useState(task.priority)
-    const [editStatus, setEditStatus] = useState(task.status)
+  const [editDueDate, setEditDueDate] = useState(
+    task.dueDate ? new Date(task.dueDate).toISOString().split("T")[0] : "",
+  );
+  const [editAssignedToId, setEditAssignedToId] = useState(
+    task.assignedTo?.id || "",
+  );
+  const [editPriority, setEditPriority] = useState(task.priority);
+  const [editStatus, setEditStatus] = useState(task.status);
 
-     const [isMetaEditing, setisMetaEditing] = useState(false)
+  const [isMetaEditing, setisMetaEditing] = useState(false);
 
-    const deleteTask = async()=>{
-      await api.delete(`/task/${workspace.id}/${task.id}`)
-       await onTaskUpdate()
-    }
+  const deleteTask = async () => {
+    await api.delete(`/task/${workspace.id}/${task.id}`);
+    await onTaskUpdate();
+  };
 
-    const updateTaskHeader = async()=>{
-        await api.put(`/task/${workspace.id}/${task.id}`,{
-             title: editTitle,
-            content: editContent
-        })
-        await onTaskUpdate()
-        setisEditing(false)
-    }
-    const updateTaskMeta = async()=>{
-        await api.put(`/task/${workspace.id}/${task.id}`,{
-             priority: editPriority,
-            status: editStatus,
-            dueDate:editDueDate  ? new Date(editDueDate).toISOString()  : null,
-             assignedToId: editAssignedToId
-        })
-        await onTaskUpdate()
-       setisMetaEditing(false)
-    }
+  const updateTaskHeader = async () => {
+    await api.put(`/task/${workspace.id}/${task.id}`, {
+      title: editTitle,
+      content: editContent,
+    });
+    await onTaskUpdate();
+    setisEditing(false);
+  };
+
+  const updateTaskMeta = async () => {
+    await api.put(`/task/${workspace.id}/${task.id}`, {
+      priority: editPriority,
+      status: editStatus,
+      dueDate: editDueDate ? new Date(editDueDate).toISOString() : null,
+      assignedToId: editAssignedToId,
+    });
+    await onTaskUpdate();
+    setisMetaEditing(false);
+  };
 
   return (
-   <div className="fixed inset-0 z-50 flex justify-end bg-black/50 backdrop-blur-sm">
-
+    <div className="fixed inset-0 z-50 flex justify-end bg-black/50 backdrop-blur-sm">
       <div className="h-full w-[42%] min-w-[550px] border-l border-zinc-800 bg-zinc-950 shadow-2xl">
-
         {/* HEADER */}
 
         <div className="flex items-center justify-between border-b border-zinc-800 px-6 py-5">
-
-     <div>
-                 {isEditing?(
-             <input
-      type="text"
-      value={editTitle}
-      onChange={(e) => setEditTitle(e.target.value)}
-      className="w-full
+          <div>
+            {isEditing ? (
+              <input
+                type="text"
+                value={editTitle}
+                onChange={(e) => setEditTitle(e.target.value)}
+                className="w-full
 bg-transparent
 text-xl
 font-semibold
@@ -65,70 +67,57 @@ outline-none
 border-b
 border-zinc-700
 pb-1"
-    />
-                 ):(
-            <h2 className="text-xl font-semibold text-zinc-100">
-              {task.title}
-            </h2>
-                 )
-                }
+              />
+            ) : (
+              <h2 className="text-xl font-semibold text-zinc-100">
+                {task.title}
+              </h2>
+            )}
 
-
-            <p className="mt-1 text-sm text-zinc-500">
-              Task Details
-            </p>
+            <p className="mt-1 text-sm text-zinc-500">Task Details</p>
           </div>
 
-         <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2">
+            {isEditing ? (
+              <>
+                <button
+                  onClick={() => setisEditing(false)}
+                  className="rounded-xl border border-none px-4 py-2 text-sm text-zinc-300 hover:bg-red-800"
+                >
+                  Cancel
+                </button>
 
-{isEditing?(
-  <>
-  
-       <button 
-    onClick={() => setisEditing(false)}
-    className="rounded-xl border border-none px-4 py-2 text-sm text-zinc-300 hover:bg-red-800"
-  >
-    Cancel
-  </button>
+                <button
+                  onClick={updateTaskHeader}
+                  className="rounded-xl border border-none px-4 py-2 text-sm text-zinc-300 hover:bg-cyan-800"
+                >
+                  Save
+                </button>
+              </>
+            ) : (
+              <button
+                onClick={() => setisEditing(true)}
+                className="rounded-xl border border-none px-4 py-2 text-sm text-zinc-300 hover:bg-zinc-800"
+              >
+                <PencilIcon size={14} />
+              </button>
+            )}
 
-       <button
-    onClick={updateTaskHeader}
-    className="rounded-xl border border-none px-4 py-2 text-sm text-zinc-300 hover:bg-cyan-800"
-  >
-    Save
-  </button>
-
-
-  </>
-):(
-  <button
-    onClick={() => setisEditing(true)}
-    className="rounded-xl border border-none px-4 py-2 text-sm text-zinc-300 hover:bg-zinc-800"
-  >
-    <PencilIcon size={14}/>
-  </button>
-)}
-
-  <button
-    onClick={onClose}
-    className="rounded-xl p-2 text-zinc-500 hover:bg-zinc-800 hover:text-zinc-300"
-  >
-    <X size={18} />
-  </button>
-
-</div>
-
-
+            <button
+              onClick={onClose}
+              className="rounded-xl p-2 text-zinc-500 hover:bg-zinc-800 hover:text-zinc-300"
+            >
+              <X size={18} />
+            </button>
+          </div>
         </div>
 
         {/* BODY */}
 
         <div className="h-[calc(100vh-90px)] overflow-y-auto p-6 space-y-6 custom-scrollbar">
-
           {/* STATUS + PRIORITY */}
 
           <div className="flex gap-3">
-
             <span className="rounded-full border border-orange-500/20 bg-orange-500/10 px-3 py-1 text-xs text-orange-400">
               {task.priority}
             </span>
@@ -136,17 +125,16 @@ pb-1"
             <span className="rounded-full border border-blue-500/20 bg-blue-500/10 px-3 py-1 text-xs text-blue-400">
               {task.status}
             </span>
-
           </div>
 
           {/* DESCRIPTION */}
-               
-               {isEditing?(
-                    <textarea
-      rows={6}
-      value={editContent}
-      onChange={(e) => setEditContent(e.target.value)}
-      className="w-full
+
+          {isEditing ? (
+            <textarea
+              rows={6}
+              value={editContent}
+              onChange={(e) => setEditContent(e.target.value)}
+              className="w-full
 bg-transparent
 text-sm
 leading-relaxed
@@ -155,46 +143,56 @@ outline-none
 resize-none
 custom-scrollbar
 "
-    />
-               ):(<>
-               
-               
-          <div className="rounded-3xl border border-zinc-800 bg-zinc-900/40 p-5 min-h-[220px]">
+            />
+          ) : (
+            <>
+              <div className="rounded-3xl border border-zinc-800 bg-zinc-900/40 p-5 min-h-[180px]">
+                <h3 className="mb-3 text-sm font-medium text-zinc-300">
+                  Description
+                </h3>
 
-            <h3 className="mb-3 text-sm font-medium text-zinc-300">
-              Description
-            </h3>
-
-            <p className="leading-relaxed text-sm text-zinc-500 break-all whitespace-pre-wrap">
-              {task.content || "No description provided."}
-            </p>
-
-          </div>
-               </>
-               )}
+                <p className="leading-relaxed text-sm text-zinc-500 break-all whitespace-pre-wrap">
+                  {task.content || "No description provided."}
+                </p>
+              </div>
+            </>
+          )}
 
           {/* META */}
 
           <div className="rounded-3xl border border-zinc-800 bg-zinc-900/40 p-5">
+            <div className="flex items-center justify-between px-2 py-1">
+              <h3 className="mb-5 text-sm font-medium text-zinc-300">
+                Details
+              </h3>
+                        
+                        {isMetaEditing?(<>
+                        <div className="flex gap-2">
+  <button
+    onClick={() => setisMetaEditing(false)}
+    className="rounded-lg px-3 py-1.5 text-xs text-zinc-400 hover:bg-zinc-800"
+  >
+    Cancel
+  </button>
 
-                    <div className='flex items-center justify-between px-2 py-1'>
-            <h3 className="mb-5 text-sm font-medium text-zinc-300">
-              Details
-            </h3>
+  <button
+    onClick={updateTaskMeta}
+    className="rounded-lg bg-zinc-100 px-3 py-1.5 text-xs font-medium text-zinc-900 hover:bg-white"
+  >
+    Save
+  </button>
+</div>
+                        </>):(
+              <button  onClick={()=>setisMetaEditing(true)}
+              className="rounded-xl border border-none px-4 py-2 text-sm text-zinc-300 hover:bg-zinc-800">
+                <PencilIcon size={14} />
+              </button>
+                        )}
+            </div>
 
-            <button className='rounded-xl border border-none px-4 py-2 text-sm text-zinc-300 hover:bg-zinc-800'> 
-                  <PencilIcon size={14}/>
-            </button>
-                           
-
-                    </div>
-
-                           
 
             <div className="space-y-5">
-
               <div className="flex items-center justify-between">
-
                 <div className="flex items-center gap-3 text-zinc-500">
                   <User size={16} />
                   <span>Assigned To</span>
@@ -203,11 +201,9 @@ custom-scrollbar
                 <span className="text-sm text-zinc-200">
                   {task.assignedTo?.name || "Unassigned"}
                 </span>
-
               </div>
 
               <div className="flex items-center justify-between">
-
                 <div className="flex items-center gap-3 text-zinc-500">
                   <Calendar size={16} />
                   <span>Due Date</span>
@@ -218,130 +214,93 @@ custom-scrollbar
                     ? new Date(task.dueDate).toLocaleDateString()
                     : "No Due Date"}
                 </span>
-
               </div>
 
               <div className="flex items-center justify-between">
-
                 <div className="flex items-center gap-3 text-zinc-500">
                   <Flag size={16} />
                   <span>Priority</span>
                 </div>
 
-                <span className="text-sm text-zinc-200">
-                  {task.priority}
-                </span>
-
+                <span className="text-sm text-zinc-200">{task.priority}</span>
               </div>
               <div className="flex items-center justify-between">
-
                 <div className="flex items-center gap-3 text-zinc-500">
                   <Flag size={16} />
                   <span>Status</span>
                 </div>
 
-                <span className="text-sm text-zinc-200">
-                  {task.status}
-                </span>
-
+                <span className="text-sm text-zinc-200">{task.status}</span>
               </div>
-
             </div>
-
           </div>
 
           {/* ACTIVITY */}
 
           <div className="rounded-3xl border border-zinc-800 bg-zinc-900/40 p-5">
-
-            <h3 className="mb-4 text-sm font-medium text-zinc-300">
-              Activity
-            </h3>
+            <h3 className="mb-4 text-sm font-medium text-zinc-300">Activity</h3>
 
             <div className="space-y-4">
-
               <div className="border-l border-zinc-700 pl-4">
-
-                <p className="text-sm text-zinc-300">
-                  Task Created
-                </p>
+                <p className="text-sm text-zinc-300">Task Created</p>
 
                 <p className="text-xs text-zinc-500">
                   {new Date(task.createdAt).toLocaleString()}
                 </p>
-
               </div>
-
             </div>
-
           </div>
 
           {/* comments  */}
-          <div className='p-3 border-b border-zinc-800'>
-             <div className="rounded-3xl border border-zinc-800 bg-zinc-950/30 p-5">
+          <div className="p-3 border-b border-zinc-800">
+            <div className="rounded-3xl border border-zinc-800 bg-zinc-950/30 p-5">
+              <h3 className="mb-4 text-sm font-medium text-zinc-300">
+                Comments
+              </h3>
 
-  <h3 className="mb-4 text-sm font-medium text-zinc-300">
-    Comments
-  </h3>
+              {/* Existing Comments */}
 
-  {/* Existing Comments */}
+              <div className="space-y-4 mb-5">
+                <div className="border-l border-zinc-700 pl-4">
+                  <div className="flex items-center gap-2">
+                    <span className="text-sm font-medium text-zinc-200">
+                      Zaib
+                    </span>
 
-  <div className="space-y-4 mb-5">
+                    <span className="text-xs text-zinc-500">2 hours ago</span>
+                  </div>
 
-    <div className="border-l border-zinc-700 pl-4">
-      <div className="flex items-center gap-2">
-        <span className="text-sm font-medium text-zinc-200">
-          Zaib
-        </span>
+                  <p className="mt-2 text-sm text-zinc-400">
+                    Authentication issue seems fixed.
+                  </p>
+                </div>
+              </div>
 
-        <span className="text-xs text-zinc-500">
-          2 hours ago
-        </span>
-      </div>
+              {/* Add Comment */}
 
-      <p className="mt-2 text-sm text-zinc-400">
-        Authentication issue seems fixed.
-      </p>
-    </div>
+              <div className="space-y-3">
+                <textarea
+                  rows={3}
+                  placeholder="Write a comment..."
+                  className="w-full resize-none rounded-2xl border border-zinc-800 bg-zinc-950 px-4 py-3 text-sm outline-none focus:border-zinc-700"
+                />
 
-  </div>
-
-  {/* Add Comment */}
-
-  <div className="space-y-3">
-
-    <textarea
-      rows={3}
-      placeholder="Write a comment..."
-      className="w-full resize-none rounded-2xl border border-zinc-800 bg-zinc-950 px-4 py-3 text-sm outline-none focus:border-zinc-700"
-    />
-
-    <div className="flex justify-end">
-
-      <button
-        className="rounded-xl bg-zinc-100 px-4 py-2 text-sm font-medium text-zinc-900 hover:bg-white"
-      >
-        Comment
-      </button>
-
-    </div>
-
-  </div>
-
-</div>
-</div>
-{/* delete button  */}
-<button
- className="w-full rounded-2xl border border-red-500/20 bg-red-500/10 py-3 text-red-400 hover:bg-red-500/20">
- Delete Task
-</button>
-
+                <div className="flex justify-end">
+                  <button className="rounded-xl bg-zinc-100 px-4 py-2 text-sm font-medium text-zinc-900 hover:bg-white">
+                    Comment
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* delete button  */}
+          <button className="w-full rounded-2xl border border-red-500/20 bg-red-500/10 py-3 text-red-400 hover:bg-red-500/20">
+            Delete Task
+          </button>
         </div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default TaskCard
+export default TaskCard;
