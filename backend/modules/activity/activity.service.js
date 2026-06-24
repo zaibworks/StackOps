78c6e53a482ecae1afd583c64b5fbfb87c8704b6
m0hammadzaib <zaibfr4@gmail.com>
2026-06-24 12:08:21 +0530

    created getActivities in activity.serivce.js

--- backend/modules/activity/activity.service.js
+++ backend/modules/activity/activity.service.js
@@ -1,1 +1,25 @@
 import prisma from "../../src/db.js";
+
+export const getActivities = async(userId)=>{
+    const memberships = await prisma.membership.findMany({
+      where:{
+        userId
+      },select:{
+        workspace:true
+      }
+    })
+ const workspaceIds =
+memberships.map(m=>m.workspaceId)
+
+return await prisma.activity.findMany({
+  where:{
+    workspaceId:{
+      in: workspaceIds
+    }
+  },
+  orderBy:{
+    createdAt:"desc"
+  },
+  take:20
+})
+}
