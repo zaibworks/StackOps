c603cf3757348a71a821560939cf650a542e9818
m0hammadzaib <zaibfr4@gmail.com>
2026-07-08 16:08:23 +0530

    added db url

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -6,6 +6,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env["DIRECT_URL"],
+    url: process.env["DATABASE_URL"],
   },
 });
