ca6b09908a8353c50c708b51fbec37f67c621fa2
m0hammadzaib <zaibfr4@gmail.com>
2026-07-08 14:55:49 +0530

    added vercel url to backend

--- backend/src/main.js
+++ backend/src/main.js
@@ -15,7 +15,7 @@ const PORT = 3000;
 app.use(cors({
   origin: [
     'http://localhost:5173', // local development
-    'https://your-frontend.vercel.app' // production frontend
+    'https://stack-6ha887kco-zaib1.vercel.app/' // production frontend
   ]
 }));
 
