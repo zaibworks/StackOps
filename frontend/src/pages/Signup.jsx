f91e9761e04ed2954ac1cca279cb81679ff5694c
m0hammadzaib <zaibfr4@gmail.com>
2026-07-10 15:19:01 +0530

    updated error handling flow

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -25,7 +25,8 @@ const Signup = () => {
           // localStorage.setItem('token',token)
           navigate('/login')
        }catch(e){
-       setError(e.response?.data?.message )
+       setError(e.response?.data?.message)
+       console.log(e.response?.data?.error?.message)
        }finally{
         setLoading(false)
        }
