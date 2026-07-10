e780bd97303f8a85c220f5d7d5dbfbfd73dcfac0
m0hammadzaib <zaibfr4@gmail.com>
2026-07-10 14:59:48 +0530

    updated signup zod schema

--- backend/modules/auth/auth.schema.js
+++ backend/modules/auth/auth.schema.js
@@ -2,7 +2,10 @@ import {email, z} from "zod";
 
 export const signupSchema = z.object({
     email: z.string().email("Invalid email"),
-    password :z.string().min(6,"Password must be 6 characters"),
+    password :z.string({
+  required_error: "Password is required",
+  invalid_type_error: "Password should contain Numbers and letters"
+}).min(6, "Password must be at least 6 characters long"),
     name: z.string().optional()
 })
 
