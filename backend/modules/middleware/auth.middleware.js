bda0ec9edeb5042605b6655a4c0af65ec23d7235
m0hammadzaib <zaibfr4@gmail.com>
2026-07-18 16:44:25 +0530

    changed something

--- backend/modules/middleware/auth.middleware.js
+++ backend/modules/middleware/auth.middleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const authMiddleware = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-
+ 
     if (!authHeader) {
       return res.status(401).json({ message: "No token provided" });
     }
