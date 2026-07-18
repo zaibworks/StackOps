655c19d2bd20ec46ed38fc873da983cd535d8c29
m0hammadzaib <zaibfr4@gmail.com>
2026-07-18 16:54:27 +0530

    added comment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const api = axios.create({
     baseURL:'http://localhost:5001/'
 })
+// added some comment in typescript branch
 // baseURL:import.meta.env.VITE_API_URL
 console.log("API URL =", import.meta.env.VITE_API_URL);
 
