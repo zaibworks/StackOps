c494d54a6c26f04d2c597c27d4ebe32871e066d9
m0hammadzaib <zaibfr4@gmail.com>
2026-07-19 18:55:06 +0530

    converted db.js to db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,11 @@ import "dotenv/config";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString: string | undefined = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is missing.");
+}
 
 const adapter = new PrismaNeon({ connectionString });
 
