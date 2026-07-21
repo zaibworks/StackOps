7ec21b321e59e7a66fc40f5a161fe6567cf203a2
m0hammadzaib <zaibfr4@gmail.com>
2026-07-21 15:45:30 +0530

    --name

--- backend/src/modules/comment/comment.schema.ts
+++ backend/src/modules/comment/comment.schema.ts
@@ -4,3 +4,5 @@ import z from "zod";
 export const commentSchema = z.object({
     content: z.string().trim().min(1,"Comment cannot be empty").max(250,"Comment is too long")
 })
+
+const CommentInput = z.infer<typeof commentSchema>
