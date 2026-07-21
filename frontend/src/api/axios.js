d1ee952785a73554ea0f4e1095b7060ed4fa9e7f
m0hammadzaib <zaibfr4@gmail.com>
2026-07-22 01:39:46 +0530

    updated axios.js in frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://localhost:5001/'
+    baseURL:import.meta.env.VITE_API_URL
 })
 // added some comment in typescript branch
-// baseURL:import.meta.env.VITE_API_URL
+// baseURL:'http://localhost:5001/'
 console.log("API URL =", import.meta.env.VITE_API_URL);
 
 //interceptors
