a699d522601548ba672828f4b0ccd49c98c81aef
m0hammadzaib <zaibfr4@gmail.com>
2026-07-23 00:58:35 +0530

    auth.controllers.js done

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,22 +1,29 @@
 import { signup, login } from './auth.service.js';
+import type { Request,Response } from 'express';
 
-const signupController =async(req,res)=>{
+const signupController =async(req:Request,res:Response)=>{
     try{
       const result = await signup(req.body)
        res.json(result)
     }catch(error){
+      if(error instanceof Error){
         res.status(400).json({ message: error.message })
+      }
     }
-}
+  }
 
-const loginController = async (req, res) => {
+
+const loginController = async (req:Request, res:Response) => {
   try {
    const result = await login(req.body)
    res.json(result)
 
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    if(error instanceof Error){
+      res.status(400).json({ message: error.message })
+    }
   }
 }
+
 
 export {signupController,loginController}
