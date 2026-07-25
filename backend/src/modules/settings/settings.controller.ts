fac748b7b65b63a8b11c7b5464411d0848148c4b
m0hammadzaib <zaibfr4@gmail.com>
2026-07-26 00:35:46 +0530

    settings.controller.ts done

--- backend/src/modules/settings/settings.controller.ts
+++ backend/src/modules/settings/settings.controller.ts
@@ -12,86 +12,109 @@ import {
     getMyActivities
 
 } from "./settings.service.js";
+import type { Response } from "express";
+import type { AuthenticatedReq } from "../../types/auth.types.js";
 
-export const updateProfileNameController = async (req,res)=>{
+export const updateProfileNameController = async (req:AuthenticatedReq,res:Response)=>{
  try {
     const userId = req.user.userId
     const {name} = req.body
     const user = await updateProfileName(userId,name)
      res.status(200).json({message:"Name changed successfully",data:user})
  } catch (e) {
+  if(e instanceof Error){
     res.status(400).json({message:e.message})
- }
+  }
+}
 }
 
-export const updatePasswordController = async(req,res)=>{
+
+export const updatePasswordController = async(req:AuthenticatedReq,res:Response)=>{
     try {
         const userId = req.user.userId
         const {currentPassword,newPassword} = req.body
         const user = await updatePassword(currentPassword,userId,newPassword)
          res.status(200).json({message:"Password reset successfully",data:user})
     } catch (e) {
+      if(e instanceof Error){
         res.status(400).json({message:e.message})
+      }
     }
-}
+  }
 
-export const deleteSelectedWorkspaceController =async(req,res)=>{
+
+export const deleteSelectedWorkspaceController =async(req:AuthenticatedReq,res:Response)=>{
     try {
         const userId = req.user.userId
         const {workspaceIds} = req.body
         const workspace = await deleteSelectedWorkspaces(userId,workspaceIds)
           res.status(200).json({message:"Workspaces deleted successfully"})
     } catch (e) {
+      if(e instanceof Error){
         res.status(500).json({message:e.message})
+      }
     }
-}
+  }
+        
 
-export const deleteSelectedTasksController = async(req,res)=>{
+export const deleteSelectedTasksController = async(req:AuthenticatedReq,res:Response)=>{
     try {
          const userId = req.user.userId
           const {taskIds} = req.body
           const tasks = await deleteSelectedTasks(userId,taskIds)
          res.status(200).json({message:"Tasks deleted successfully"})
     } catch (e) {
-         res.status(500).json({message:e.message})
+      if(e instanceof Error){
+        res.status(500).json({message:e.message})
+      }
     }
-}
+  }
 
-export const deleteSelectedCommentsController = async(req,res)=>{
+
+export const deleteSelectedCommentsController = async(req:AuthenticatedReq,res:Response)=>{
     try {
          const userId = req.user.userId
           const {commentIds} = req.body
           const comment = await deleteSelectedComments(userId,commentIds)
          res.status(200).json({message:"Comments deleted successfully"})
     } catch (e) {
-         res.status(500).json({message:e.message})
+      if(e instanceof Error){
+        res.status(500).json({message:e.message})
+      }
     }
-}
+  }
 
-export const deleteSelectedActivitiesController = async(req,res)=>{
+
+export const deleteSelectedActivitiesController = async(req:AuthenticatedReq,res:Response)=>{
     try {
          const userId = req.user.userId
           const {activityIds} = req.body
           const activities = await deleteSelectedActivities(userId,activityIds)
          res.status(200).json({message:"Activities deleted successfully"})
     } catch (e) {
-         res.status(500).json({message:e.message})
+      if(e instanceof Error){
+        res.status(500).json({message:e.message})
+      }
     }
-}
+  }
 
-export const deleteUserAccountController = async(req,res)=>{
+
+export const deleteUserAccountController = async(req:AuthenticatedReq,res:Response)=>{
   try {
     const userId = req.user.userId
     const {currentPassword} = req.body
     const removed = await deleteUserAccount(userId,currentPassword)
      res.status(200).json({message:"Account permanently deleted"})
   } catch (e) {
-    res.status(402).json({message:e.message})
+    if(e instanceof Error){
+      res.status(402).json({message:e.message})
+    }
   }
 }
+      
 
 
-export const getOwnedWorkspacesController = async (req, res) => {
+export const getOwnedWorkspacesController = async (req:AuthenticatedReq,res:Response) => {
   try {
     const userId = req.user.userId;
 
@@ -102,13 +125,14 @@ export const getOwnedWorkspacesController = async (req, res) => {
       data: workspaces,
     });
   } catch (e) {
-    res.status(400).json({
-      message: e.message,
-    });
+    if(e instanceof Error){
+      res.status(400).json({message: e.message,});
+    }
   }
 };
+      
 
-export const getMyTasksController = async (req, res) => {
+export const getMyTasksController = async (req:AuthenticatedReq, res:Response) => {
   try {
     const userId = req.user.userId;
 
@@ -119,13 +143,14 @@ export const getMyTasksController = async (req, res) => {
       data: tasks,
     });
   } catch (e) {
-    res.status(400).json({
-      message: e.message,
-    });
+    if(e instanceof Error){
+      res.status(400).json({message: e.message,});
+    }
   }
 };
 
-export const getMyCommentsController = async (req, res) => {
+
+export const getMyCommentsController = async (req:AuthenticatedReq, res:Response) => {
   try {
     const userId = req.user.userId;
 
@@ -136,14 +161,15 @@ export const getMyCommentsController = async (req, res) => {
       data: comments,
     });
   } catch (e) {
-    res.status(400).json({
-      message: e.message,
-    });
+    if(e instanceof Error){
+      res.status(400).json({ message: e.message,});
+    }
   }
 };
+    
 
 
-export const getMyActivitiesController = async (req, res) => {
+export const getMyActivitiesController = async (req:AuthenticatedReq, res:Response) => {
   try {
     const userId = req.user.userId;
 
@@ -154,8 +180,9 @@ export const getMyActivitiesController = async (req, res) => {
       data: activities,
     });
   } catch (e) {
-    res.status(400).json({
-      message: e.message,
-    });
+    if(e instanceof Error){
+      res.status(400).json({message: e.message,});
+    }
   }
 };
+
